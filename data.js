593a75de525d5d8b40aa2c1714c0a043ec2ceb81
ErwinScramble <erwin.alexander@scramble.nl>
2026-04-25 01:39:57 +0200

    Add Embraer Emb326 data with coordinates and info

--- data.js
+++ data.js
@@ -15,5 +15,22 @@ const permanentData = {
         }
       ]
     }
+  },
+  "Embraer": {
+    "Emb326": {
+      "img": "MB326.png",
+      "dots": [
+        {
+          "x": 46,
+          "y": 40.0408149819552,
+          "info": "Cockpit bulkhead, behind the rear seat."
+        },
+        {
+          "x": 82.125,
+          "y": 51.37954029995026,
+          "info": "Fuselage, left side below the stabilizer."
+        }
+      ]
+    }
   }
 };
