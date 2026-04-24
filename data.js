28a7df565e4167cc9f4ea03185a317a853a33fac
ErwinScramble <erwin.alexander@scramble.nl>
2026-04-25 01:27:09 +0200

    Add permanentData structure with Aermacchi details

--- data.js
+++ data.js
@@ -1,1 +1,19 @@
-const permanentData = {};
+const permanentData = {
+  "Aermacchi": {
+    "MB326": {
+      "img": "MB326.png",
+      "dots": [
+        {
+          "x": 46,
+          "y": 39.81404393589966,
+          "info": "Cockpit bulkhead, behind the rear seat."
+        },
+        {
+          "x": 83.125,
+          "y": 52.05986727933431,
+          "info": "Fuselage, left side below the stabilizer."
+        }
+      ]
+    }
+  }
+};
